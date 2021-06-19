0b2776a664cebf60659bbc45f0e4796e844cffb2
condee78 <condee78@gmail.com>
2021-06-19 23:57:35 +0700

    add users feature

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
 require("dotenv").config();
 
 const Hapi = require("@hapi/hapi");
+
+// open-music
 const openMusic = require("./api/open-music");
 const OpenMusicService = require("./services/postgres/OpenMusicService");
 const OpenMusicValidator = require("./validator/open-music");
 
+// users
+const users = require("./api/users");
+const UsersService = require("./services/postgres/UsersService");
+const UsersValidator = require("./validator/users");
+
 const init = async () => {
   const openMusicService = new OpenMusicService();
+  const usersService = new UsersService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -18,13 +26,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: openMusic,
-    options: {
-      service: openMusicService,
-      validator: OpenMusicValidator,
+  await server.register([
+    {
+      plugin: openMusic,
+      options: {
+        service: openMusicService,
+        validator: OpenMusicValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
